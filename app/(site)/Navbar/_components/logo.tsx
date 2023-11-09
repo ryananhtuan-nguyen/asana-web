85652214750d8465ca84e4827cdacd5a5b889b06
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-09 16:14:44 +1300

    logo component

--- app/(site)/Navbar/_components/logo.tsx
+++ app/(site)/Navbar/_components/logo.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 const Logo = () => {
-  return <div>Logo</div>
+  return (
+    <Link href="/">
+      <Image
+        src="/images/logo/logo.svg"
+        alt="logo"
+        width={80}
+        height={80}
+        className="mr-6"
+      />
+    </Link>
+  )
 }
 
 export default Logo
